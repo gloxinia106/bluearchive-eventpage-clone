2f48de5470a88de8f44d8a3ada5b6ab0a61f1cfa
gloxinia106 <gloxinia106@gmail.com>
2023-02-04 13:10:46 +0900

    section0

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,28 @@
 import Section0 from "@/router/section0";
 import ReactFullpage from "@fullpage/react-fullpage";
+import { useState } from "react";
 
 export default function Home() {
+  const [youtubeModal, useYoutubeModal] = useState();
   return (
-    <ReactFullpage
-      licenseKey={"YOURE Key"}
-      loopBottom={true}
-      render={({ state, fullpageApi }) => {
-        return (
-          <ReactFullpage.Wrapper>
-            <Section0 />
-            <div className="section">
-              <div>Sectton2</div>
-            </div>
-            <div className="section">
-              <div>Sectton3</div>
-            </div>
-          </ReactFullpage.Wrapper>
-        );
-      }}
-    />
+    <>
+      <ReactFullpage
+        licenseKey={"YOURE Key"}
+        loopBottom={true}
+        render={({ state, fullpageApi }) => {
+          return (
+            <ReactFullpage.Wrapper>
+              <Section0 fullpageApi={fullpageApi} />
+              <div className="section">
+                <div>Sectton2</div>
+              </div>
+              <div className="section">
+                <div>Sectton3</div>
+              </div>
+            </ReactFullpage.Wrapper>
+          );
+        }}
+      />
+    </>
   );
 }
--- router/section0.tsx
+++ router/section0.tsx
@@ -1,5 +1,75 @@
-import style from "@/styles/Section0.module.css";
+import { fullpageApi } from "@fullpage/react-fullpage";
+import { useRef, useState } from "react";
 
-export default function Section0() {
-  return <div className="section items-center">hello</div>;
+interface Section0Props {
+  fullpageApi: fullpageApi;
+}
+
+export default function Section0({ fullpageApi }: Section0Props) {
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+  const [musicBtn, setMusicBtn] = useState(false);
+
+  return (
+    <div id="section0" className="section">
+      <video
+        id="bgVideo"
+        data-autoplay
+        playsInline
+        loop
+        muted
+        className="absolute top-0 object-cover w-full h-full"
+      >
+        <source src="/current/videos/bg_video_ko.mp4"></source>
+      </video>
+      <div className="absolute top-[20px] right-[25px] z-30 flex">
+        <div className="hover:cursor-pointer snsLink">
+          <div className="bg-[url('/current/img_sprite.png')] w-[95px] h-[80px] bg-[-236px_-286px]"></div>
+          <ul className="bg-[url('/current/img_sprite.png')] w-[95px] h-[270px] bg-[-435px_-286px] hidden">
+            <li className="w-[82px] h-[71px]"></li>
+            <li className="w-[82px] h-[64px]">
+              <a
+                className="block w-full h-full"
+                href="https://www.facebook.com/"
+              ></a>
+            </li>
+            <li className="w-[82px] h-[64px]">
+              <a
+                className="block w-full h-full"
+                href="https://twitter.com/KR_BlueArchive?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"
+              ></a>
+            </li>
+            <li className="w-[82px] h-[64px]">
+              <a
+                className="block w-full h-full"
+                href="https://bluearchive.nexon.com/events/2023/01/reg"
+              ></a>
+            </li>
+          </ul>
+        </div>
+        <audio ref={audioRef} src="/bgm.mp3" hidden />
+        <button
+          className={`bg-[url('/current/img_sprite.png')] w-[77px] h-[80px] ${
+            musicBtn ? "bg-[-331px_-286px]" : "bg-[-530px_-286px]"
+          }`}
+          onClick={() => {
+            setMusicBtn(!musicBtn);
+            musicBtn ? audioRef.current?.pause() : audioRef.current?.play();
+          }}
+        ></button>
+      </div>
+      <div className="bg-black w-full h-full absolute top-0 opacity-70 z-10"></div>
+      <div className="w-full h-full absolute top-0 z-20 flex flex-col justify-center items-center">
+        <div className="bg-[url('/current/section0/title_ko.png')] w-[940px] h-[355px] bg-[-255px_0px]"></div>
+        <button className="bg-[url('/current/section0/btn_play.png')] w-[121px] h-[138px] up-animation" />
+        <div className="bg-[url('/current/section0/desc_ko.png')] w-[475px] h-[65px] bg-[-491px_-500px]"></div>
+        <button
+          className="bg-[url('/current/section0/btn_goReg.png')] w-[450px] h-[105px] up-animation"
+          onClick={() => {
+            fullpageApi.moveSectionDown();
+          }}
+        ></button>
+        <div className="absolute bottom-0 bg-[url('/current/bul_scroll.png')] w-[45px] h-[60px] animate-bounce"></div>
+      </div>
+    </div>
+  );
 }
