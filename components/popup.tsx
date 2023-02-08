f4236f2b53220340a7da73193e204138327911c2
gloxinia106 <gloxinia106@gmail.com>
2023-02-08 16:51:37 +0900

    popup animation

--- components/popup.tsx
+++ components/popup.tsx
@@ -1,5 +1,6 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { createPortal } from "react-dom";
+import { motion, AnimatePresence } from "framer-motion";
 
 interface PopupProps {
   children: ReactNode;
@@ -7,24 +8,44 @@ interface PopupProps {
   handleClose: () => void;
 }
 
+const popupVariants = {
+  start: { scale: 0, opacity: 0 },
+  end: {
+    scale: 1,
+    opacity: 1,
+    transition: { duration: 0.5, type: "spring", bounce: 0.5 },
+  },
+  exit: { scale: 0, opacity: 0, transition: { duration: 0.2 } },
+};
+
 export default function Popup({ isOpen, children, handleClose }: PopupProps) {
-  return isOpen
+  const [htmlElement, setHtmlElement] = useState<HTMLElement | null>(null);
+  useEffect(() => {
+    setHtmlElement(document.getElementById("notice-popup"));
+  });
+  return htmlElement
     ? createPortal(
-        <>
-          <div
-            className="fixed top-0 left-0 w-full h-screen bg-black bg-opacity-40 flex items-center justify-center"
-            onClick={handleClose}
-          >
-            <div
-              onClick={(e) => {
-                e.stopPropagation();
-              }}
+        <AnimatePresence>
+          {isOpen && (
+            <motion.div
+              className="fixed top-0 left-0 w-full h-screen bg-black bg-opacity-40 flex items-center justify-center"
+              onClick={handleClose}
             >
-              {children}
-            </div>
-          </div>
-        </>,
-        document.getElementById("notice-popup")!
+              <motion.div
+                onClick={(e) => {
+                  e.stopPropagation();
+                }}
+                variants={popupVariants}
+                initial="start"
+                animate="end"
+                exit="exit"
+              >
+                {children}
+              </motion.div>
+            </motion.div>
+          )}
+        </AnimatePresence>,
+        htmlElement!
       )
     : null;
 }
